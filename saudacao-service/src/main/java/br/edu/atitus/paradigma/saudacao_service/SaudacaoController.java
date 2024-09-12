00bb49d137bb317f0fd76d1f33169e03e768ed81
Vitor Quadros <vitoralgusto472@gmail.com>
2024-09-12 20:51:34 -0300

    Realizado criação da @RestController, para que o navegador retorne o valor -Olá mundou- ou se adicionado parâmetros ?name=vitor, retorna Ola Vitor

---
+++ saudacao-service/src/main/java/br/edu/atitus/paradigma/saudacao_service/SaudacaoController.java
@@ -0,0 +1,25 @@
+package br.edu.atitus.paradigma.saudacao_service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saudacao-service")
+public class SaudacaoController {
+	
+	@GetMapping({"", "/", "/{nomePath}"})
+	public ResponseEntity<String> getSaudacao(
+			@RequestParam(required = false, name = "name") String nome,
+			@PathVariable (required = false) String nomePath){
+		if (nome == null ) 
+			nome = nomePath == null ? "World" : nomePath;
+		String template = "%s %s!";
+		return ResponseEntity.ok(String.format(template, "hello", nome ));
+		
+	}
+	
+}
