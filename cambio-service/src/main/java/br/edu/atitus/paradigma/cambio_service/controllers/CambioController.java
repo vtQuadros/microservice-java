e4e080befe2fc615da816640399e801b36e5e058
Vitor Quadros <vitoralgusto472@gmail.com>
2024-11-03 20:05:13 -0300

    [200~Implementando resiliência no nosso cambio-service através do Circuit Breaker

--- cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
+++ cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/controllers/CambioController.java
@@ -13,6 +13,7 @@ import br.edu.atitus.paradigma.cambio_service.clients.CotacaoClient;
 import br.edu.atitus.paradigma.cambio_service.clients.CotacaoResponse;
 import br.edu.atitus.paradigma.cambio_service.entities.CambioEntity;
 import br.edu.atitus.paradigma.cambio_service.repositories.CambioRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -31,18 +32,19 @@ public class CambioController {
 	private int porta;
 	
 	@GetMapping("/{valor}/{origem}/{destino}")
+	@CircuitBreaker(name = "cotacaoClient", fallbackMethod = "getCambioFromDB")
 	public ResponseEntity<CambioEntity> getCambio(
 			@PathVariable double valor,
 			@PathVariable String origem,
 			@PathVariable String destino) throws Exception {
 		
-		// CambioEntity cambio = cambioRepository.findByOrigemAndDestino(origem, destino)
-			//	.orElseThrow(() -> new Exception("Câmbio não encontrado para esta origem e destino"));
+		
 		
 		CambioEntity cambio = new CambioEntity();
 		cambio.setOrigem(origem);
 		cambio.setDestino(destino);
 		double fator;
+		System.out.println("Tentativa via Banco Central");
 		CotacaoResponse CotacaoOrigem = cotacaoClient.getCotacao(origem, "10-17-2024");
 		double fatorOrigem = CotacaoOrigem.getValue().get(0).getCotacaoVenda();
 		
@@ -60,6 +62,20 @@ public class CambioController {
 		cambio.setAmbiente("Cambio-Service run in port: " + porta);
 		return ResponseEntity.ok(cambio);
 	}
+	
+	public ResponseEntity<CambioEntity> getCambioFromDB(
+			double valor,
+			String origem,
+			String destino,
+			Throwable e) throws Exception {
+		
+		CambioEntity cambio = cambioRepository.findByOrigemAndDestino(origem, destino)
+			.orElseThrow(() -> new Exception("Câmbio não encontrado para esta origem e destino"));
+		cambio.setValorConvertido(valor * cambio.getFator());
+		cambio.setAmbiente("Cambio-Service run in port: " + porta + "(From DB)");
+		return ResponseEntity.ok(cambio);
+	}
+	
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> handleException(Exception e) {
